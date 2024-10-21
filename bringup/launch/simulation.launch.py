b780f295a82523d144d4f135bcbc03c40e82aac8
mathpz <matheus.pedrozo@gmail.com>
2024-10-21 13:02:47 -0300

    added joystick launch and configs

--- bringup/launch/simulation.launch.py
+++ bringup/launch/simulation.launch.py
@@ -34,17 +34,16 @@ def generate_launch_description():
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(package_name), 'launch', 'gazebo.launch.py'
-        )]), launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-    )
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+             )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawner_entity = Node(
-        package='gazebo_ros',
-        executable='spawn_entity',
-        arguments=['-topic', 'robot_description', '-entity', 'hestia'],
-        output='screen'
-    )
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'hestia'],
+                        output='screen')
 
     diff_drive_spawner = Node(
         package="controller_manager",
@@ -64,7 +63,7 @@ def generate_launch_description():
         joystick,
         twist_mux,
         gazebo,
-        spawner_entity,
+        spawn_entity,
         diff_drive_spawner,
         joint_broad_spawner
     ])
