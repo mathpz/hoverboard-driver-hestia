eeb901d6d2dd1281584e9c64198d1fc264c8ca1f
mathpz <matheus.pedrozo@gmail.com>
2024-11-12 11:34:22 -0300

    work

--- bringup/launch/rplidar.launch.py
+++ bringup/launch/rplidar.launch.py
@@ -11,6 +11,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+                #'serial_port': '/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0',
                 'frame_id': 'lidar_link',
                 'angle_compensate': True,
                 'scan_mode': 'Express',
